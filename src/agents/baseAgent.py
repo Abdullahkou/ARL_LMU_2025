8254f82d7db121a63c8cec16fee6ef930c272e87
marc <marc.a.fichtner@gmail.com>
2025-08-30 14:37:27 +0200

    basedir

--- src/agents/baseAgent.py
+++ src/agents/baseAgent.py
@@ -81,20 +81,20 @@ class IAgent(Protocol):
         """
         pass
 
-    def save(self, path: str) -> None:
+    def save(self, base_dir: str) -> None:
         """
         Save the agent's state to a file.
 
         :algo_name: name of the algorithm
-        :param path: path to save the agent's state to
+        :param base_dir: base_dir to save the agent's state to
         """
         pass
 
-    def load(self, path: str) -> None:
+    def load(self, base_dir: str) -> None:
         """
         Load the agent's state from a file.
 
-        :param path: path to load the agent's state from
+        :param base_dir: base_dir to load the agent's state from
         """
         pass
 
