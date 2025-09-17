63b398e7f5c84afd89919ccbab5e8d1a603517db
Fichtner, Marc <marc.a.fichtner@gmail.com>
2025-09-17 12:08:28 +0200

    can now recalc seed means in hindsight

--- src/recalc_seed_means.py
+++ src/recalc_seed_means.py
@@ -11,25 +11,31 @@ from config.training_config import (
     VALIDATION_RESULTS_FILE,
     Algorithm,
     load_training_config,
+    save_training_config,
 )
 from utils.logging import TRAINING_COLS, aggregate_head_results, save_seed_totals
 
 
-def recalc_seed_means(model_dir: str):
+def recalc_seed_means(model_dir: str, seeds_to_collect: list[int]):
     """
-    Recalculate mean results across seeds for both training and validation.
+    Recalculate mean results across seeds from individuals seed results.
     """
 
     config = load_training_config(model_dir)
-    seeds = config.seeds
+    config.seeds = seeds_to_collect
+    save_training_config(config=config, dir=model_dir)
+
+    seeds_dir = f"{model_dir}/{SEEDS_DIR}"
 
     # ---------- VALIDATION ----------
     validation_seed_results = list[np.ndarray]()
     validation_intervals = None
-    for seed in seeds:
-        val_file = f"{model_dir}/{SEEDS_DIR}/seed_{seed}/{VALIDATION_RESULTS_FILE}"
-        if not os.path.exists(val_file):
-            continue
+    for seed in seeds_to_collect:
+        val_file = f"{seeds_dir}/seed_{seed}/{VALIDATION_RESULTS_FILE}"
+
+        assert os.path.exists(val_file), (
+            f"couldnt locate {val_file} for seeds {seeds_to_collect}"
+        )
 
         df = pd.read_csv(val_file, index_col=0)
 
@@ -49,8 +55,8 @@ def recalc_seed_means(model_dir: str):
     if config.record_training:
         training_seed_results = list[np.ndarray]()
         training_intervals = None
-        for seed in seeds:
-            train_file = f"{model_dir}/{SEEDS_DIR}/seed_{seed}/{TRAINING_RESULTS_FILE}"
+        for seed in seeds_to_collect:
+            train_file = f"{seeds_dir}/seed_{seed}/{TRAINING_RESULTS_FILE}"
             if not os.path.exists(train_file):
                 continue
 
@@ -79,13 +85,15 @@ def recalc_seed_means(model_dir: str):
         aggregate_head_results(
             agent_dir=model_dir,
             num_heads=config.algo_config.hyper_params.n_q_heads,
-            seeds=seeds,
+            seeds=seeds_to_collect,
         )
 
 
 def main():
-    model_dir = "results/test/Walker2d-v5/1.5M/RANDOM"
-    recalc_seed_means(model_dir)
+    model_dir = "results/test/LunarLander-v3/1.0K/RANDOM_0"
+
+    seeds_to_collect = [0, 1]
+    recalc_seed_means(model_dir, seeds_to_collect=seeds_to_collect)
 
 
 if __name__ == "__main__":
