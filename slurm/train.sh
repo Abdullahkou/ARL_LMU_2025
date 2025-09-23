4d7807135621ac8253d3b052725a9d87c4983641
Marc Fichtner <marc.a.fichtner@gmail.com>
2025-09-23 18:02:55 +0200

    new train setup

--- slurm/train.sh
+++ slurm/train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J DQN_3_auto
+#SBATCH -J DQN_3
 #SBATCH -D ./
 #SBATCH --partition=All
 
@@ -11,5 +11,14 @@ cd ..
 
 uv version
 
-# dont forget to adjust the postfix!! 3qh_bpauto
-uv run src/train.py --save_postfix="3qh_bpauto" algo_config.hyper_params.n_q_heads=3 algo_config.algorithm=Custom_DQN env_id=LunarLander-v3 seeds=[0,1,2,3,4,5,6,7,8,9] steps=1000000
+# dont forget to adjust the postfix!! 3qh
+# [0,1,2,3,4,5,6,7,8,9]
+uv run src/train.py --save_postfix="3qh" \
+ algo_config.hyper_params.n_q_heads=3 \
+ seeds=[0,1,2,3,4,5,6,7,8,9] \
+ algo_config.hyper_params.hidden_sizes=[64,64] \
+ algo_config.hyper_params.bootstrap_prob=1.0 \
+ algo_config.hyper_params.independent_heads=False \
+ env_id=LunarLander-v3 \
+ algo_config.algorithm=Custom_DQN \
+ steps=1000000
